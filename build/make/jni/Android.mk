8d4dbf2b165f50b7f6fe70838409cdc0dee94556
twinaphex <libretro@gmail.com>
2018-05-28 08:11:23 +0200

    Update makefile skeleton

---
+++ build/make/jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+CORE_DIR := $(LOCAL_PATH)/..
+
+include $(LOCAL_PATH)/../Makefile.common
+
+COREFLAGS := $(COREDEFINES) -DPSS_STYLE=1 $(INCFLAGS)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+	COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+include $(BUILD_SHARED_LIBRARY)
